5ed0a7126ee01ec2317e88e9f9c5fc8113ee3e29
Shilomalu <hilotomalu@gmail.com>
2025-10-10 14:59:16 +0900

    feat: Add responsive design for mobile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">タイムライン</RouterLink>
+        <RouterLink to="/timeline">タイムライン</RouterLink>
         <RouterLink to="/post">投稿する</RouterLink>
         <RouterLink to="/register">新規登録</RouterLink>
         <RouterLink to="/login">ログイン</RouterLink>
