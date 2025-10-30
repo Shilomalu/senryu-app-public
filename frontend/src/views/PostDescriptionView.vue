9eee62be1f94df5ac7e72bfedab554e522a5802a
Si184 <taikonoshin@gmail.com>
2025-10-30 20:06:56 +0900

    ミス修正

--- frontend/src/views/PostDescriptionView.vue
+++ frontend/src/views/PostDescriptionView.vue
@@ -12,19 +12,3 @@ const returnPost = () => {
   <p>・記号　:　。、「」・！？</p>
   <button @click="returnPost">戻る</button>
 </template>
-
-<style>
-button {
-  width: 20%;
-  padding: 10px;
-  font-size: 1em;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 6px;
-  cursor: pointer;
-}
-button:hover {
-  background-color: #0056b3;
-}
-</style>
