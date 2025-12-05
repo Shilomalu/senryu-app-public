c498838b40636742458193f4f57ffc85872a517c
Shilomalu <hilotomalu@gmail.com>
2025-12-05 22:06:28 +0900

    huronnto

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -9,7 +9,7 @@ const router = useRouter();
 
 const handleLogin = async () => {
   try {
-    const res = await fetch('http://localhost:3001/api/users/login', {
+    const res = await fetch('/api/users/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
